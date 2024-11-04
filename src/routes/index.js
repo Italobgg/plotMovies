6eef95b7d9d82ab9d7e3b9d4ea5a137e19b5a8a9
Italo BGG <italobggdev@gmail.com>
2024-11-04 18:32:12 -0300

    add rota

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,9 @@ import express from "express";
 import MovieController from "../controllers/MovieController.js";
 
 const routes = (app) => {
-    app.route("/").get((req, res) => res.status(200).send("Rota inicial"));
-    app.use(express.json());
-    app.get("/movie/search", MovieController.getMoviePlot);
-}
+  app.route("/").get((req, res) => res.status(200).send("Rota inicial"));
+  app.use(express.json());
+  app.get("/movie/search", MovieController.getMoviePlot);
+};
 
 export default routes;
